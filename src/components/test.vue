5abdc208142ba5fcab97a76a5e5b5f3d45e3d6ab
txl <txl-pc@txldeMacBook-Pro.local>
2017-09-07 10:17:46 +0800

    添加组件

--- src/components/test.vue
+++ src/components/test.vue
@@ -114,6 +114,85 @@
 
             <input type="submit" value="提交">
         </form>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="text">文字按钮</el-button>
+        <el-button :plain="true" :disabled="true">主要按钮</el-button>
+        <el-button type="primary" :disabled="true">主要按钮</el-button>
+        <el-button type="text" :disabled="true">文字按钮</el-button>
+        <div class="block">
+            <span class="demonstration">默认显示颜色</span>
+            <span class="wrapper">
+                <el-button type="success">成功按钮</el-button>
+                <el-button type="warning">警告按钮</el-button>
+                <el-button type="danger">危险按钮</el-button>
+                <el-button type="info">信息按钮</el-button>
+            </span>
+        </div>
+        <div class="block">
+            <span class="demonstration">hover 显示颜色</span>
+            <span class="wrapper">
+                <el-button :plain="true" type="success">成功按钮</el-button>
+                <el-button :plain="true" type="warning">警告按钮</el-button>
+                <el-button :plain="true" type="danger">危险按钮</el-button>
+                <el-button :plain="true" type="info">信息按钮</el-button>
+            </span>
+        </div>
+        <el-button  :plain="true" type="primary" icon="edit"></el-button>
+        <el-button type="primary" icon="share"></el-button>
+        <el-button type="primary" icon="delete"></el-button>
+        <el-button type="primary" icon="search">搜索</el-button>
+        <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+        <el-button-group>
+            <el-button type="primary" icon="arrow-left">上一页</el-button>
+            <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+        </el-button-group>
+        <el-button-group>
+            <el-button type="primary" icon="edit"></el-button>
+            <el-button type="primary" icon="share"></el-button>
+            <el-button type="primary" icon="delete"></el-button>
+        </el-button-group>
+        <el-button type="primary" :loading="true">加载中</el-button>
+        <el-button type="primary" size="large">大型按钮</el-button>
+        <el-button type="primary">正常按钮</el-button>
+        <el-button type="primary" size="small">小型按钮</el-button>
+        <el-button type="primary" size="mini">超小按钮</el-button>
+        <el-radio class="radio" v-model="radio" label="1">备选项1</el-radio>
+        <el-radio class="radio" v-model="radio" label="2">备选项2</el-radio>
+        <el-radio disabled v-model="radio1" label="label1">备选项</el-radio>
+        <el-radio disabled v-model="radio1" label="label2">备选项</el-radio>
+        <el-radio-group v-model="radio2">
+            <el-radio :label="3">备选项</el-radio>
+            <el-radio :label="6">备选项</el-radio>
+            <el-radio :label="9">备选项</el-radio>
+        </el-radio-group>
+        <el-radio-group v-model="radio3">
+            <el-radio :label="3">备选1</el-radio>
+            <el-radio :label="6">备选2项</el-radio>
+            <el-radio :label="9">备选3项</el-radio>
+        </el-radio-group>
+        <div style="margin: 15px 0;"></div>
+        <el-radio-group v-model="radio4">
+            <el-radio-button label="上海"></el-radio-button>
+            <el-radio-button label="北京"></el-radio-button>
+            <el-radio-button label="广州"></el-radio-button>
+            <el-radio-button label="深圳"></el-radio-button>
+        </el-radio-group>
+        <div style="margin: 15px 0;"></div>
+        <el-radio-group v-model="radio5">
+            <el-radio-button label="上海"></el-radio-button>
+            <el-radio-button label="北京" :disabled="true"></el-radio-button>
+            <el-radio-button label="广州"></el-radio-button>
+            <el-radio-button label="深圳"></el-radio-button>
+        </el-radio-group>
+        <div style="margin: 15px 0;"></div>
+        <el-radio-group v-model="radio6" :disabled="true">
+            <el-radio-button label="上海"></el-radio-button>
+            <el-radio-button label="北京"></el-radio-button>
+            <el-radio-button label="广州"></el-radio-button>
+            <el-radio-button label="深圳"></el-radio-button>
+            <el-checkbox v-model="checked">备选项</el-checkbox>
+        </el-radio-group>
     </div>
 </template>
 
@@ -124,7 +203,15 @@
       return {
         user: {
           gender: ''
-        }
+        },
+        radio: '',
+        radio1: 'label1',
+        radio2: 3,
+        radio3: 9,
+        radio4: '上海',
+        radio5: '北京',
+        radio6: '深圳',
+        checked: true
       }
     },
     created () {
